310645b47347e0610bff772fa930acb45daab372
yzffvinz <1178043794@qq.com>
2021-12-16 22:43:09 +0800

    feat: reset

--- portal/static/fetch.js
+++
@@ -1,9 +0,0 @@
-function r() {
-    fetch('http://localhost:8080/index')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (myJson) {
-            console.log(myJson);
-        });
-}
--- server.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = exports = {
-    port: 8080
-};
